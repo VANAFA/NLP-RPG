11d1dc103ce5cd7f83f2fba46ae47e9c7cb55385
Vanafa <vfadel@udesa.edu.ar>
2026-06-12 22:47:09 -0300

    ok 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // (or vue, svelte, etc.)
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4000,
-    strictPort: true,
-  },
-  preview: {
-    port: 4000,
-    strictPort: true,
-  },
-});
+  base: '/NLP-RPG/',
+})
