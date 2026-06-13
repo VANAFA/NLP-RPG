92307a6ddf0143b2f7cbcbc973c27555513e853b
VANAFA <127407846+VANAFA@users.noreply.github.com>
2026-06-12 23:01:48 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react' // (or vue, svelte, etc.)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NLP-RPG/',
+  base: '/NLP-RPG/', 
 })
